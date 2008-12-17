9f4db0ac81b1eeaa8e67aefb94a63f222c4aac24
David Nickerson <d.nickerson@auckland.ac.nz>
2008-12-17 16:11:50 +0000

    further work on openCMISS(cm) CellML interfaces. Starting to deviate a little from the original powerpoint definitions
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/branches/developers/andre@281 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- src/cmiss_cellml.f90
+++ src/cmiss_cellml.f90
@@ -67,7 +67,7 @@ CONTAINS
     INTEGER(INTG), INTENT(IN) :: CELLML_USER_NUMBER !<Not sure what the purpose of this is? I guess to give the created CellML environment object a unique identifier? Perhaps if the same identifier is specified then an existing CellML environment would be reused/reinitialised for the given source field...
     TYPE(FIELD_TYPE), POINTER :: FIELD !<The (source) field to set up this CellML environment for.
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The newly created CellML environment object.
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
 
     CALL ENTERS("CELLML_CREATE_START",ERR,ERROR,*999)
@@ -82,7 +82,7 @@ CONTAINS
   !! Check the provided CellML environment object and if it all looks good clear the "in progress" flag to indicate the object is now ready for use.
   SUBROUTINE CELLML_CREATE_FINISH(CELLML,ERR,ERROR,*)
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object to check and finialise creation of.
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
 
     CALL ENTERS("CELLML_CREATE_FINISH",ERR,ERROR,*999)
@@ -97,7 +97,7 @@ CONTAINS
   !! Ensure the provided CellML environment object is suitable and ready for creating all the individual models that will be used with it. Probably simply check the object is valid and empty of all models? or perhaps it also a way to indicate that the user wants to add some more models to it?
   SUBROUTINE CELLML_MODELS_CREATE_START(CELLML,ERR,ERROR,*)
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object that we want to start creating models with.
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
 
     CALL ENTERS("CELLML_MODELS_CREATE_START",ERR,ERROR,*999)
@@ -114,7 +114,7 @@ CONTAINS
   !! - perhaps we simply "validate" the models to ensure they are suitable for use before the user gets too far along? or maybe that should be done with the model import routine?
   SUBROUTINE CELLML_MODELS_CREATE_FINISH(CELLML,ERR,ERROR,*)
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object that we want to finalise the creation of new models with.
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
 
     CALL ENTERS("CELLML_MODELS_CREATE_FINISH",ERR,ERROR,*999)
@@ -133,7 +133,7 @@ CONTAINS
     INTEGER(INTG), INTENT(IN) :: MODEL_USER_NUMBER !<The unique identifier for this model within the given CellML environment object.
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object into which we want to import the specified model.
     TYPE(VARYING_STRING), INTENT(IN) :: URI !<The (absolute? relative?) URI of the model to import. This should be a fully qualified URI which resolves to either a CellML XML model element or locates CellML simulation metadata (openCMISS CellML related metadata?). In the case of simulation metadata, the model will be completely defined from the metadata. If a bare model is found then the default simulation parameters are used? or the user is prompted for more data? or an error is flagged?
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
 
     CALL ENTERS("CELLML_MODEL_IMPORT",ERR,ERROR,*999)
@@ -150,7 +150,7 @@ CONTAINS
   SUBROUTINE CELLML_MODELS_FIELD_CREATE_START(MODEL_FIELD_USER_NUMBER,CELLML,ERR,ERROR,*)
     INTEGER(INTG), INTENT(IN) :: MODEL_FIELD_USER_NUMBER !<The unique identifier for the models field to be created for the given CellML environment object.
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which we need to create the models field.
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
 
     CALL ENTERS("CELLML_MODELS_FIELD_CREATE_START",ERR,ERROR,*999)
@@ -165,7 +165,7 @@ CONTAINS
   !! This will finalise the creation of the models field for the given CellML environment object.
   SUBROUTINE CELLML_MODELS_FIELD_CREATE_FINISH(CELLML,ERR,ERROR,*)
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which we need to finish creation of the models field.
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
 
     CALL ENTERS("CELLML_MODELS_FIELD_CREATE_FINISH",ERR,ERROR,*999)
@@ -181,7 +181,7 @@ CONTAINS
   SUBROUTINE CELLML_MODELS_FIELD_GET(CELLML,FIELD,ERR,ERROR,*)
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object from which to get the models field.
     TYPE(FIELD_TYPE), POINTER :: FIELD !<On successful return will be set to the models field for this CellML environment
-    TYPE(INTG), INTENT(OUT) :: ERR !<The error code.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
     TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
 
     CALL ENTERS("CELLML_MODELS_FIELD_GET",ERR,ERROR,*999)
@@ -191,5 +191,78 @@ CONTAINS
     CALL EXITS("CELLML_MODELS_FIELD_GET")
     RETURN 1
   END SUBROUTINE CELLML_MODELS_FIELD_GET
+
+  !> Start the creation of the state field for the given CellML environment.
+  !! We want to create a single field which will be used to store the state variables of each of the models in the given CellML environment at each of the DOFs for the environment's base field.
+  !! - one field for all models with a component in the field for each variable
+  !! - can a single field have different components at different DOFs (grid points)?
+  !! - perhaps need a state field for each model and each field is only defined at the DOFs valid for that model?
+  !! - this has to be called after the models field is defined.
+  SUBROUTINE CELLML_STATE_FIELD_CREATE_START(STATE_FIELD_USER_NUMBER,CELLML,ERR,ERROR,*)
+    INTEGER(INTG), INTENT(IN) :: STATE_FIELD_USER_NUMBER !<The unique identifier for the state field to be created for the given CellML environment object.
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which to create the state field.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_STATE_FIELD_CREATE_START",ERR,ERROR,*999)
+    CALL EXITS("CELLML_STATE_FIELD_CREATE_START")
+    RETURN
+999 CALL ERRORS("CELLML_STATE_FIELD_CREATE_START",ERR,ERROR)
+    CALL EXITS("CELLML_STATE_FIELD_CREATE_START")
+    RETURN 1
+  END SUBROUTINE CELLML_STATE_FIELD_CREATE_START
+
+  !> Finialse the creation of the state field for the given CellML environment.
+  !! Finish creating the state variable field for the provided CellML environment.
+  !! - default field values are set for all components in the field at all DOFs based on the initial_value's from the CellML model
+  SUBROUTINE CELLML_STATE_FIELD_CREATE_FINISH(CELLML,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which to finalise the creation of the state field.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_STATE_FIELD_CREATE_FINISH",ERR,ERROR,*999)
+    CALL EXITS("CELLML_STATE_FIELD_CREATE_FINISH")
+    RETURN
+999 CALL ERRORS("CELLML_STATE_FIELD_CREATE_FINISH",ERR,ERROR)
+    CALL EXITS("CELLML_STATE_FIELD_CREATE_FINISH")
+    RETURN 1
+  END SUBROUTINE CELLML_STATE_FIELD_CREATE_FINISH
+
+  !> Fetch the state field for the given CellML environment.
+  !! Check the state field is correctly defined and return it for the user to access - either for getting the current values or to set initial conditions.
+  !! - need some way to get from variable URIs in the CellML models to components in the returned state field.
+  !! - standard field routines should be used to get/set field component values.
+  SUBROUTINE CELLML_STATE_FIELD_GET(CELLML,FIELD,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object from which to get the state field.
+    TYPE(FIELD_TYPE), POINTER :: FIELD !<On successful return will be set to the state field for this CellML environment
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_STATE_FIELD_GET",ERR,ERROR,*999)
+    CALL EXITS("CELLML_STATE_FIELD_GET")
+    RETURN
+999 CALL ERRORS("CELLML_STATE_FIELD_GET",ERR,ERROR)
+    CALL EXITS("CELLML_STATE_FIELD_GET")
+    RETURN 1
+  END SUBROUTINE CELLML_STATE_FIELD_GET
+
+  !> Find the component ID in the given field for the variable defined by the given URI in the provided CellML environment.
+  !! This generic routine will be used to map variable URI's in CellML models to components in the various fields defined in the CellML models defined for the provided CellML environment.
+  !! - may need to also provide a FIELD_VARIABLE_NUMBER (always 1?) for completeness
+  SUBROUTINE CELLML_FIELD_COMPONENT_GET(CELLML,FIELD,URI,COMPONENT_USER_NUMBER,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object from which to get the field component.
+    TYPE(FIELD_TYPE), POINTER :: FIELD !<The field in which to locate the specific component. No need to know whether this is the state, algebraic, or parameter field?
+    TYPE(VARYING_STRING), INTENT(IN) :: URI !<The URI of the model variable which needs to be located in the provided field.
+    INTEGER(INTG), INTENT(OUT) :: COMPONENT_USER_NUMBER !<On successful return will be the component user number of the field component for the model variable defined by the given URI.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_FIELD_COMPONENT_GET",ERR,ERROR,*999)
+    CALL EXITS("CELLML_FIELD_COMPONENT_GET")
+    RETURN
+999 CALL ERRORS("CELLML_FIELD_COMPONENT_GET",ERR,ERROR)
+    CALL EXITS("CELLML_FIELD_COMPONENT_GET")
+    RETURN 1
+  END SUBROUTINE CELLML_FIELD_COMPONENT_GET
 
 END MODULE CMISS_CELLML
