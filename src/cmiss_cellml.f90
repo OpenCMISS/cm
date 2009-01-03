5c7c9919a818eb90f4ac7d3dfe1bd6767bfee171
David Nickerson <d.nickerson@auckland.ac.nz>
2009-01-03 10:51:36 +0000

    further additions to the openCMISS-CellML API, defining how users will interact with intermediate and parameter variables.
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/branches/developers/andre@285 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- src/cmiss_cellml.f90
+++ src/cmiss_cellml.f90
@@ -110,7 +110,7 @@ CONTAINS
 
   !> Finalise the given CellML environment after all models have been defined.
   !! This routine is called once all the models for the provided CellML environment have been created.
-  !! - is this where the models are instantiated? (turned into code and compiled)
+  !! - is this where the models are instantiated? (turned into code and compiled) - no, need more information before we can generate code.
   !! - perhaps we simply "validate" the models to ensure they are suitable for use before the user gets too far along? or maybe that should be done with the model import routine?
   SUBROUTINE CELLML_MODELS_CREATE_FINISH(CELLML,ERR,ERROR,*)
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object that we want to finalise the creation of new models with.
@@ -194,7 +194,7 @@ CONTAINS
 
   !> Start the creation of the state field for the given CellML environment.
   !! We want to create a single field which will be used to store the state variables of each of the models in the given CellML environment at each of the DOFs for the environment's base field.
-  !! - one field for all models with a component in the field for each variable
+  !! - one field for all models with a component in the field for each variable (perhaps simply use the largest model to set the number of components for all DOFs and some will not be used).
   !! - can a single field have different components at different DOFs (grid points)?
   !! - perhaps need a state field for each model and each field is only defined at the DOFs valid for that model?
   !! - this has to be called after the models field is defined.
@@ -249,6 +249,7 @@ CONTAINS
   !> Find the component ID in the given field for the variable defined by the given URI in the provided CellML environment.
   !! This generic routine will be used to map variable URI's in CellML models to components in the various fields defined in the CellML models defined for the provided CellML environment.
   !! - may need to also provide a FIELD_VARIABLE_NUMBER (always 1?) for completeness
+  !! - is the model URI also needed?
   SUBROUTINE CELLML_FIELD_COMPONENT_GET(CELLML,FIELD,URI,COMPONENT_USER_NUMBER,ERR,ERROR,*)
     TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object from which to get the field component.
     TYPE(FIELD_TYPE), POINTER :: FIELD !<The field in which to locate the specific component. No need to know whether this is the state, algebraic, or parameter field?
@@ -264,5 +265,172 @@ CONTAINS
     CALL EXITS("CELLML_FIELD_COMPONENT_GET")
     RETURN 1
   END SUBROUTINE CELLML_FIELD_COMPONENT_GET
+
+  !> Create a field used to store intermediate variables of interest.
+  !! The intermediate field created for this environment provides access to the value of nominated intermediate variables from the models available in this CellML environment.
+  !! - this field will be similar in structure to the state field, but only contains the nominated variables.
+  SUBROUTINE CELLML_INTERMEDIATE_FIELD_CREATE_START(INTERMEDIATE_FIELD_USER_NUMBER,CELLML,ERR,ERROR,*)
+    INTEGER(INTG), INTENT(IN) :: INTERMEDIATE_FIELD_USER_NUMBER !<The unique identifier for the intermediate field to be created for the given CellML environment object.
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object from which to get the field component.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_INTERMEDIATE_FIELD_CREATE_START",ERR,ERROR,*999)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_CREATE_START")
+    RETURN
+999 CALL ERRORS("CELLML_INTERMEDIATE_FIELD_CREATE_START",ERR,ERROR)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_CREATE_START")
+    RETURN 1
+  END SUBROUTINE CELLML_INTERMEDIATE_FIELD_CREATE_START
+
+  !> Finialse the creation of the intermediate field for the given CellML environment.
+  !! Finish creating the intermediate variable field for the provided CellML environment.
+  !! - check for valid variables being defined?
+  !! - maybe delete the field if no valid variables/components exist?
+  SUBROUTINE CELLML_INTERMEDIATE_FIELD_CREATE_FINISH(CELLML,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which to finalise the creation of the intermediate field.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_INTERMEDIATE_FIELD_CREATE_FINISH",ERR,ERROR,*999)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_CREATE_FINISH")
+    RETURN
+999 CALL ERRORS("CELLML_INTERMEDIATE_FIELD_CREATE_FINISH",ERR,ERROR)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_CREATE_FINISH")
+    RETURN 1
+  END SUBROUTINE CELLML_INTERMEDIATE_FIELD_CREATE_FINISH
+
+  !> Add a specific variable to this CellML environment's intermediate field.
+  !! Nominate a specific variable from a model in this environment for inclusion in the environment's intermediate field. This will ensure the value of this variable is available at each of the environment's (source) field DOF's for which the specified model is valid.
+  SUBROUTINE CELLML_INTERMEDIATE_FIELD_ADD(CELLML,MODEL_URI,VARIABLE_URI,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which to add the nominated variable to the intermediate field.
+    TYPE(VARYING_STRING), INTENT(IN) :: MODEL_URI !<The URI of the model (simulation?) in which to look for the variable being nominated.
+    TYPE(VARYING_STRING), INTENT(IN) :: VARIABLE_URI !<The URI of the variable in the specified model to include in the intermediate field for this CellML environment.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_INTERMEDIATE_FIELD_ADD",ERR,ERROR,*999)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_ADD")
+    RETURN
+999 CALL ERRORS("CELLML_INTERMEDIATE_FIELD_ADD",ERR,ERROR)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_ADD")
+    RETURN 1
+  END SUBROUTINE CELLML_INTERMEDIATE_FIELD_ADD
+
+  !> Fetch the intermediate field for the given CellML environment.
+  !! Check the intermediate field is correctly defined and return it for the user to access.
+  !! - is there a way to ensure the field returned to the calling routine is "read-only"?
+  SUBROUTINE CELLML_INTERMEDIATE_FIELD_GET(CELLML,FIELD,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object from which to get the intermediate field.
+    TYPE(FIELD_TYPE), POINTER :: FIELD !<On successful return will be set to the intermediate field for this CellML environment
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_INTERMEDIATE_FIELD_GET",ERR,ERROR,*999)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_GET")
+    RETURN
+999 CALL ERRORS("CELLML_INTERMEDIATE_FIELD_GET",ERR,ERROR)
+    CALL EXITS("CELLML_INTERMEDIATE_FIELD_GET")
+    RETURN 1
+  END SUBROUTINE CELLML_INTERMEDIATE_FIELD_GET
+
+  !> Start the parameters definition process.
+  !! Initialise the CellML environment ready for the nomination of parameters from this environment's models that will be overridden by field values.
+  !! - what to do if the paramters field has already be defined?
+  SUBROUTINE CELLML_PARAMATERS_CREATE_START(CELLML,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which we will be defining parameter overrides.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_PARAMATERS_CREATE_START",ERR,ERROR,*999)
+    CALL EXITS("CELLML_PARAMATERS_CREATE_START")
+    RETURN
+999 CALL ERRORS("CELLML_PARAMATERS_CREATE_START",ERR,ERROR)
+    CALL EXITS("CELLML_PARAMATERS_CREATE_START")
+    RETURN 1
+  END SUBROUTINE CELLML_PARAMATERS_CREATE_START
+
+  !> Finialse the parameters definition process.
+  !! Indicates that the user has added all parameter overrides and allows the CellML environment to be further processed.
+  SUBROUTINE CELLML_PARAMETERS_CREATE_FINISH(CELLML,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which to finalise the parameter overrides.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_PARAMETERS_CREATE_FINISH",ERR,ERROR,*999)
+    CALL EXITS("CELLML_PARAMETERS_CREATE_FINISH")
+    RETURN
+999 CALL ERRORS("CELLML_PARAMETERS_CREATE_FINISH",ERR,ERROR)
+    CALL EXITS("CELLML_PARAMETERS_CREATE_FINISH")
+    RETURN 1
+  END SUBROUTINE CELLML_PARAMETERS_CREATE_FINISH
+
+  !> Nominate a specific parameter in CellML environment to override.
+  !! Nominate a specific parameter variable from a model in this environment which will have its value overridden by the envronment's parameter field.
+  SUBROUTINE CELLML_PARAMETERS_ADD(CELLML,MODEL_URI,VARIABLE_URI,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object.
+    TYPE(VARYING_STRING), INTENT(IN) :: MODEL_URI !<The URI of the model (simulation?) in which to look for the variable being nominated.
+    TYPE(VARYING_STRING), INTENT(IN) :: VARIABLE_URI !<The URI of the variable in the specified model to specify for overriding.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_PARAMETERS_ADD",ERR,ERROR,*999)
+    CALL EXITS("CELLML_PARAMETERS_ADD")
+    RETURN
+999 CALL ERRORS("CELLML_PARAMETERS_ADD",ERR,ERROR)
+    CALL EXITS("CELLML_PARAMETERS_ADD")
+    RETURN 1
+  END SUBROUTINE CELLML_PARAMETERS_ADD
+
+  !> Create the parameters field for this CellML environment.
+  !! Check that parameters have been nominated and create the environment's user accessible parameters field. The user accessible parameters field is used by the user to define parameter overrides and is distinct from the internally used parameters field which is always defined for the environment's (source) field DOF's.
+  !! - similar to the models field.
+  !! - should initialise all components of the field as constant fields with a value as specified in the corresponding CellML models imported into the environment?
+  SUBROUTINE CELLML_PARAMATERS_FIELD_CREATE_START(PARAMETERS_FIELD_USER_NUMBER,CELLML,ERR,ERROR,*)
+    INTEGER(INTG), INTENT(IN) :: PARAMETERS_FIELD_USER_NUMBER !<The unique identifier for the parameters field to be created for the given CellML environment object.
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which we will be defining the parameters field.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_PARAMATERS_FIELD_CREATE_START",ERR,ERROR,*999)
+    CALL EXITS("CELLML_PARAMATERS_FIELD_CREATE_START")
+    RETURN
+999 CALL ERRORS("CELLML_PARAMATERS_FIELD_CREATE_START",ERR,ERROR)
+    CALL EXITS("CELLML_PARAMATERS_FIELD_CREATE_START")
+    RETURN 1
+  END SUBROUTINE CELLML_PARAMATERS_FIELD_CREATE_START
+
+  !> Finialse the parameters field definition.
+  !! - here is where we sync the user level parameters field to the internal parameters representation?
+  !! - or maybe we simply create the internal field?
+  !! - the internal field will contain compnents for all parameters in all models?
+  SUBROUTINE CELLML_PARAMETERS_FIELD_CREATE_FINISH(CELLML,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object for which to finalise the parameters field.
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_PARAMETERS_FIELD_CREATE_FINISH",ERR,ERROR,*999)
+    CALL EXITS("CELLML_PARAMETERS_FIELD_CREATE_FINISH")
+    RETURN
+999 CALL ERRORS("CELLML_PARAMETERS_FIELD_CREATE_FINISH",ERR,ERROR)
+    CALL EXITS("CELLML_PARAMETERS_FIELD_CREATE_FINISH")
+    RETURN 1
+  END SUBROUTINE CELLML_PARAMETERS_FIELD_CREATE_FINISH
+
+  !> Fetch the user parameters field for the given CellML environment.
+  !! Check the parameters field is correctly defined and return it for the user to access in order to define the desired parameter overrides.
+  SUBROUTINE CELLML_PARAMETERS_FIELD_GET(CELLML,FIELD,ERR,ERROR,*)
+    TYPE(CELLML_TYPE), POINTER :: CELLML !<The CellML environment object from which to get the prameters field.
+    TYPE(FIELD_TYPE), POINTER :: FIELD !<On successful return will be set to the parameters field for this CellML environment
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code.
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string.
+
+    CALL ENTERS("CELLML_PARAMETERS_FIELD_GET",ERR,ERROR,*999)
+    CALL EXITS("CELLML_PARAMETERS_FIELD_GET")
+    RETURN
+999 CALL ERRORS("CELLML_PARAMETERS_FIELD_GET",ERR,ERROR)
+    CALL EXITS("CELLML_PARAMETERS_FIELD_GET")
+    RETURN 1
+  END SUBROUTINE CELLML_PARAMETERS_FIELD_GET
 
 END MODULE CMISS_CELLML
