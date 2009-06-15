805704308f1d4932c93a5fb0d0c94e1bcd5d6d10
Ting Yu <ting.yu@auckland.ac.nz>
2009-06-15 03:41:58 +0000

    Build master path change
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@573 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- doc/doxygen/autotest.py
+++ doc/doxygen/autotest.py
@@ -13,8 +13,8 @@ def getLatest(path) :
       latest = i
   return str(latest)
 
-linux_intel_latest = getLatest('/home/autotest/buildmaster/intel/OpenCMISS')
-linux_gnu_latest = getLatest('/home/autotest/buildmaster/gnu/OpenCMISS')
+linux_intel_latest = getLatest('/home/autotest/opencmiss-build/buildmaster/intel/OpenCMISS')
+linux_gnu_latest = getLatest('/home/autotest/opencmiss-build/buildmaster/gnu/OpenCMISS')
 
 text = 'ALIASES += linux_intel_latest="'+linux_intel_latest+'"\nALIASES += linux_gnu_latest="'+linux_gnu_latest+'"\n'
 
