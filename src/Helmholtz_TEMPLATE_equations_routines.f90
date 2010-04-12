7d0fcad573bfb6aa371f2ee05d1c12f692431a2f
Sebastian Krittian <sebastian.krittian@comlab.ox.ac.uk>
2010-04-12 11:22:14 +0000

    Added Helmholtz_equations_routines for OpenCMISS coding week in Oxford.
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1037 61e36070-c886-4827-b7c0-ed9e4dc59cc8

---
+++ src/Helmholtz_TEMPLATE_equations_routines.f90
@@ -0,0 +1,435 @@
+!> \file
+!> $Id$
+!> \author Chris Bradley
+!> \brief This module handles all Helmholtz equations routines.
+!>
+!> \section LICENSE
+!>
+!> Version: MPL 1.1/GPL 2.0/LGPL 2.1
+!>
+!> The contents of this file are subject to the Mozilla Public License
+!> Version 1.1 (the "License"); you may not use this file except in
+!> compliance with the License. You may obtain a copy of the License at
+!> http://www.mozilla.org/MPL/
+!>
+!> Software distributed under the License is distributed on an "AS IS"
+!> basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+!> License for the specific language governing rights and limitations
+!> under the License.
+!>
+!> The Original Code is OpenCMISS
+!>
+!> The Initial Developer of the Original Code is University of Auckland,
+!> Auckland, New Zealand and University of Oxford, Oxford, United
+!> Kingdom. Portions created by the University of Auckland and University
+!> of Oxford are Copyright (C) 2007 by the University of Auckland and
+!> the University of Oxford. All Rights Reserved.
+!>
+!> Contributor(s):
+!>
+!> Alternatively, the contents of this file may be used under the terms of
+!> either the GNU General Public License Version 2 or later (the "GPL"), or
+!> the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+!> in which case the provisions of the GPL or the LGPL are applicable instead
+!> of those above. If you wish to allow use of your version of this file only
+!> under the terms of either the GPL or the LGPL, and not to allow others to
+!> use your version of this file under the terms of the MPL, indicate your
+!> decision by deleting the provisions above and replace them with the notice
+!> and other provisions required by the GPL or the LGPL. If you do not delete
+!> the provisions above, a recipient may use your version of this file under
+!> the terms of any one of the MPL, the GPL or the LGPL.
+!>
+
+!>This module handles all Helmholtz equations routines.
+MODULE HELMHOLTZ_EQUATIONS_ROUTINES
+
+  USE BASE_ROUTINES
+  USE BASIS_ROUTINES
+  USE BOUNDARY_CONDITIONS_ROUTINES
+  USE CONSTANTS
+  USE CONTROL_LOOP_ROUTINES
+  USE DISTRIBUTED_MATRIX_VECTOR
+  USE DOMAIN_MAPPINGS
+  USE EQUATIONS_ROUTINES
+  USE EQUATIONS_MAPPING_ROUTINES
+  USE EQUATIONS_MATRICES_ROUTINES
+  USE EQUATIONS_SET_CONSTANTS
+  USE FIELD_ROUTINES
+  USE INPUT_OUTPUT
+  USE ISO_VARYING_STRING
+  USE KINDS
+  USE MATRIX_VECTOR
+  USE NODE_ROUTINES
+  USE PROBLEM_CONSTANTS
+  USE STRINGS
+  USE SOLVER_ROUTINES
+  USE TIMER
+  USE TYPES
+
+  IMPLICIT NONE
+
+  PRIVATE
+
+  !Module parameters
+
+  !Module types
+
+  !Module variables
+
+  !Interfaces
+
+  PUBLIC HELMHOLTZ_EQUATION_FINITE_ELEMENT_CALCULATE,HELMHOLTZ_EQUATION_EQUATIONS_SET_SETUP, &
+    & HELMHOLTZ_EQUATION_EQUATIONS_SET_SOLUTION_METHOD_SET,HELMHOLTZ_EQUATION_EQUATIONS_SET_SUBTYPE_SET, &
+    & HELMHOLTZ_EQUATION_PROBLEM_SUBTYPE_SET,HELMHOLTZ_EQUATION_PROBLEM_SETUP
+  
+CONTAINS
+
+  !
+  !================================================================================================================================
+  !
+
+  !>Calculates the element stiffness matrices and RHS for a Helmholtz equation finite element equations set.
+  SUBROUTINE HELMHOLTZ_EQUATION_FINITE_ELEMENT_CALCULATE(EQUATIONS_SET,ELEMENT_NUMBER,ERR,ERROR,*)
+
+    !Argument variables
+    TYPE(EQUATIONS_SET_TYPE), POINTER :: EQUATIONS_SET !<A pointer to the equations set to perform the finite element calculations on
+    INTEGER(INTG), INTENT(IN) :: ELEMENT_NUMBER !<The element number to calculate
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(EQUATIONS_TYPE), POINTER :: EQUATIONS
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATION_FINITE_ELEMENT_CALCULATE",ERR,ERROR,*999)
+    
+    IF(ASSOCIATED(EQUATIONS_SET)) THEN
+      EQUATIONS=>EQUATIONS_SET%EQUATIONS
+      IF(ASSOCIATED(EQUATIONS)) THEN
+        SELECT CASE(EQUATIONS_SET%SUBTYPE)
+        CASE(EQUATIONS_SET_NO_SOURCE_HELMHOLTZ_SUBTYPE)
+
+
+          
+        CASE DEFAULT
+          LOCAL_ERROR="Equations set subtype "//TRIM(NUMBER_TO_VSTRING(EQUATIONS_SET%SUBTYPE,"*",ERR,ERROR))// &
+            & " is not valid for a Helmholtz equation type of a classical field equations set class."
+          CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+        END SELECT
+      ELSE
+        CALL FLAG_ERROR("Equations set equations is not associated.",ERR,ERROR,*999)
+      ENDIF
+    ELSE
+      CALL FLAG_ERROR("Equations set is not associated.",ERR,ERROR,*999)
+    ENDIF
+    
+    CALL EXITS("HELMHOLTZ_EQUATION_FINITE_ELEMENT_CALCULATE")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_FINITE_ELEMENT_CALCULATE",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_FINITE_ELEMENT_CALCULATE")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_FINITE_ELEMENT_CALCULATE
+
+  !
+  !================================================================================================================================
+  !
+
+  !>Sets up the Helmholtz equation type of a classical field equations set class.
+  SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_SETUP(EQUATIONS_SET,EQUATIONS_SET_SETUP,ERR,ERROR,*)
+
+    !Argument variables
+    TYPE(EQUATIONS_SET_TYPE), POINTER :: EQUATIONS_SET !<A pointer to the equations set to setup a Helmholtz equation on.
+    TYPE(EQUATIONS_SET_SETUP_TYPE), INTENT(INOUT) :: EQUATIONS_SET_SETUP !<The equations set setup information
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SETUP",ERR,ERROR,*999)
+
+    IF(ASSOCIATED(EQUATIONS_SET)) THEN
+      SELECT CASE(EQUATIONS_SET%SUBTYPE)
+      CASE(EQUATIONS_SET_NO_SOURCE_HELMHOLTZ_SUBTYPE)
+        CALL HELMHOLTZ_EQUATION_EQUATIONS_SET_LINEAR_SETUP(EQUATIONS_SET,EQUATIONS_SET_SETUP,ERR,ERROR,*999)        
+      CASE DEFAULT
+        LOCAL_ERROR="Equations set subtype "//TRIM(NUMBER_TO_VSTRING(EQUATIONS_SET%SUBTYPE,"*",ERR,ERROR))// &
+          & " is not valid for a Helmholtz equation type of a classical field equation set class."
+        CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+      END SELECT
+    ELSE
+      CALL FLAG_ERROR("Equations set is not associated.",ERR,ERROR,*999)
+    ENDIF
+       
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SETUP")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SETUP",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SETUP")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_SETUP
+
+  !
+  !================================================================================================================================
+  !
+
+  !>Sets/changes the solution method for a Helmholtz equation type of an classical field equations set class.
+  SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_SOLUTION_METHOD_SET(EQUATIONS_SET,SOLUTION_METHOD,ERR,ERROR,*)
+
+    !Argument variables
+    TYPE(EQUATIONS_SET_TYPE), POINTER :: EQUATIONS_SET !<A pointer to the equations set to set the solution method for
+    INTEGER(INTG), INTENT(IN) :: SOLUTION_METHOD !<The solution method to set
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATIONS_SET_SOLUTION_METHOD_SET",ERR,ERROR,*999)
+    
+    IF(ASSOCIATED(EQUATIONS_SET)) THEN
+      SELECT CASE(EQUATIONS_SET%SUBTYPE)
+      CASE(EQUATIONS_SET_NO_SUBTYPE)        
+        SELECT CASE(SOLUTION_METHOD)
+        CASE(EQUATIONS_SET_FEM_SOLUTION_METHOD)
+          EQUATIONS_SET%SOLUTION_METHOD=EQUATIONS_SET_FEM_SOLUTION_METHOD
+        CASE(EQUATIONS_SET_BEM_SOLUTION_METHOD)
+          CALL FLAG_ERROR("Not implemented.",ERR,ERROR,*999)
+        CASE(EQUATIONS_SET_FD_SOLUTION_METHOD)
+          CALL FLAG_ERROR("Not implemented.",ERR,ERROR,*999)
+        CASE(EQUATIONS_SET_FV_SOLUTION_METHOD)
+          CALL FLAG_ERROR("Not implemented.",ERR,ERROR,*999)
+        CASE(EQUATIONS_SET_GFEM_SOLUTION_METHOD)
+          CALL FLAG_ERROR("Not implemented.",ERR,ERROR,*999)
+        CASE(EQUATIONS_SET_GFV_SOLUTION_METHOD)
+          CALL FLAG_ERROR("Not implemented.",ERR,ERROR,*999)
+        CASE DEFAULT
+          LOCAL_ERROR="The specified solution method of "//TRIM(NUMBER_TO_VSTRING(SOLUTION_METHOD,"*",ERR,ERROR))//" is invalid."
+          CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+        END SELECT
+      CASE DEFAULT
+        LOCAL_ERROR="Equations set subtype of "//TRIM(NUMBER_TO_VSTRING(EQUATIONS_SET%SUBTYPE,"*",ERR,ERROR))// &
+          & " is not valid for a Helmholtz equation type of an classical field equations set class."
+        CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+      END SELECT
+    ELSE
+      CALL FLAG_ERROR("Equations set is not associated.",ERR,ERROR,*999)
+    ENDIF
+       
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SOLUTION_METHOD_SET")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SOLUTION_METHOD_SET",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SOLUTION_METHOD_SET")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_SOLUTION_METHOD_SET
+
+  !
+  !================================================================================================================================
+  !
+
+  !>Sets/changes the equation subtype for a Helmholtz equation type of a classical field equations set class.
+  SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_SUBTYPE_SET(EQUATIONS_SET,EQUATIONS_SET_SUBTYPE,ERR,ERROR,*)
+
+    !Argument variables
+    TYPE(EQUATIONS_SET_TYPE), POINTER :: EQUATIONS_SET !<A pointer to the equations set to set the equation subtype for
+    INTEGER(INTG), INTENT(IN) :: EQUATIONS_SET_SUBTYPE !<The equation subtype to set
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SUBTYPE_SET",ERR,ERROR,*999)
+    
+    IF(ASSOCIATED(EQUATIONS_SET)) THEN
+      SELECT CASE(EQUATIONS_SET_SUBTYPE)
+      CASE(EQUATIONS_SET_NO_SOURCE_HELMHOLTZ_SUBTYPE)
+        EQUATIONS_SET%CLASS=EQUATIONS_SET_CLASSICAL_FIELD_CLASS
+        EQUATIONS_SET%TYPE=EQUATIONS_SET_HELMHOLTZ_EQUATION_TYPE
+        EQUATIONS_SET%SUBTYPE=EQUATIONS_SET_NO_SOURCE_HELMHOLTZ_SUBTYPE
+      CASE DEFAULT
+        LOCAL_ERROR="Equations set subtype "//TRIM(NUMBER_TO_VSTRING(EQUATIONS_SET_SUBTYPE,"*",ERR,ERROR))// &
+          & " is not valid for a Helmholtz equation type of a classical field equations set class."
+        CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+      END SELECT
+    ELSE
+      CALL FLAG_ERROR("Equations set is not associated.",ERR,ERROR,*999)
+    ENDIF
+       
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SUBTYPE_SET")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SUBTYPE_SET",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_SUBTYPE_SET")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_SUBTYPE_SET
+
+  !
+  !================================================================================================================================
+  !
+
+  !>Sets up the linear Helmholtz equation.
+  SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_LINEAR_SETUP(EQUATIONS_SET,EQUATIONS_SET_SETUP,ERR,ERROR,*)
+
+    !Argument variables
+    TYPE(EQUATIONS_SET_TYPE), POINTER :: EQUATIONS_SET !<A pointer to the equations set to setup
+    TYPE(EQUATIONS_SET_SETUP_TYPE), INTENT(INOUT) :: EQUATIONS_SET_SETUP !<The equations set setup information
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATION_EQUATION_SET_LINEAR_SETUP",ERR,ERROR,*999)
+
+    IF(ASSOCIATED(EQUATIONS_SET)) THEN
+      IF(EQUATIONS_SET%SUBTYPE==EQUATIONS_SET_NO_SOURCE_HELMHOLTZ_SUBTYPE) THEN
+        SELECT CASE(EQUATIONS_SET_SETUP%SETUP_TYPE)
+        CASE(EQUATIONS_SET_SETUP_INITIAL_TYPE)
+        CASE(EQUATIONS_SET_SETUP_GEOMETRY_TYPE)
+        CASE(EQUATIONS_SET_SETUP_DEPENDENT_TYPE)
+        CASE(EQUATIONS_SET_SETUP_MATERIALS_TYPE)
+        CASE(EQUATIONS_SET_SETUP_SOURCE_TYPE)
+        CASE(EQUATIONS_SET_SETUP_ANALYTIC_TYPE)
+        CASE(EQUATIONS_SET_SETUP_EQUATIONS_TYPE)
+        CASE(EQUATIONS_SET_SETUP_BOUNDARY_CONDITIONS_TYPE)
+        CASE DEFAULT
+          LOCAL_ERROR="The setup type of "//TRIM(NUMBER_TO_VSTRING(EQUATIONS_SET_SETUP%SETUP_TYPE,"*",ERR,ERROR))// &
+            & " is invalid for a standard Helmholtz equation."
+          CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+        END SELECT
+      ELSE
+        LOCAL_ERROR="The equations set subtype of "//TRIM(NUMBER_TO_VSTRING(EQUATIONS_SET%SUBTYPE,"*",ERR,ERROR))// &
+          & " does not equal a linear Helmholtz equation subtype."
+        CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+      ENDIF
+    ELSE
+      CALL FLAG_ERROR("Equations set is not associated.",ERR,ERROR,*999)
+    ENDIF
+       
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_LINEAR_SETUP")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_EQUATIONS_SET_LINEAR_SETUP",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_EQUATIONS_SET_LINEAR_SETUP")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_EQUATIONS_SET_LINEAR_SETUP
+
+  !
+  !================================================================================================================================
+  !
+ 
+  !>Sets up the Helmholtz solution.
+  SUBROUTINE HELMHOLTZ_EQUATION_PROBLEM_SETUP(PROBLEM,PROBLEM_SETUP,ERR,ERROR,*)
+    
+    !Argument variables
+    TYPE(PROBLEM_TYPE), POINTER :: PROBLEM !<A pointer to the solutions set to setup a Helmholtz equation on.
+    TYPE(PROBLEM_SETUP_TYPE), INTENT(INOUT) :: PROBLEM_SETUP !<The problem setup information
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATION_PROBLEM_SETUP",ERR,ERROR,*999)
+
+    IF(ASSOCIATED(PROBLEM)) THEN
+      SELECT CASE(PROBLEM%SUBTYPE)
+      CASE(PROBLEM_NO_SOURCE_HELMHOLTZ_SUBTYPE)
+        CALL HELMHOLTZ_EQUATION_PROBLEM_LINEAR_SETUP(PROBLEM,PROBLEM_SETUP,ERR,ERROR,*999)
+      CASE DEFAULT
+        LOCAL_ERROR="Problem subtype "//TRIM(NUMBER_TO_VSTRING(PROBLEM%SUBTYPE,"*",ERR,ERROR))// &
+          & " is not valid for a Helmholtz equation type of a classical field problem class."
+        CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+      END SELECT
+    ELSE
+      CALL FLAG_ERROR("Problem is not associated.",ERR,ERROR,*999)
+    ENDIF
+       
+    CALL EXITS("HELMHOLTZ_EQUATION_PROBLEM_SETUP")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_PROBLEM_SETUP",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_PROBLEM_SETUP")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_PROBLEM_SETUP
+  
+  !
+  !================================================================================================================================
+  !
+
+  !>Sets/changes the problem subtype for a Helmholtz equation type .
+  SUBROUTINE HELMHOLTZ_EQUATION_PROBLEM_SUBTYPE_SET(PROBLEM,PROBLEM_SUBTYPE,ERR,ERROR,*)
+
+    !Argument variables
+    TYPE(PROBLEM_TYPE), POINTER :: PROBLEM !<A pointer to the problem to set the problem subtype for
+    INTEGER(INTG), INTENT(IN) :: PROBLEM_SUBTYPE !<The problem subtype to set
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATION_PROBLEM_SUBTYPE_SET",ERR,ERROR,*999)
+    
+    IF(ASSOCIATED(PROBLEM)) THEN
+      SELECT CASE(PROBLEM_SUBTYPE)
+      CASE(PROBLEM_NO_SOURCE_HELMHOLTZ_SUBTYPE)        
+        PROBLEM%CLASS=PROBLEM_CLASSICAL_FIELD_CLASS
+        PROBLEM%TYPE=PROBLEM_HELMHOLTZ_EQUATION_TYPE
+        PROBLEM%SUBTYPE=PROBLEM_NO_SOURCE_HELMHOLTZ_SUBTYPE     
+      CASE DEFAULT
+        LOCAL_ERROR="Problem subtype "//TRIM(NUMBER_TO_VSTRING(PROBLEM_SUBTYPE,"*",ERR,ERROR))// &
+          & " is not valid for a Helmholtz equation type of a classical field problem class."
+        CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+      END SELECT
+    ELSE
+      CALL FLAG_ERROR("Problem is not associated.",ERR,ERROR,*999)
+    ENDIF
+       
+    CALL EXITS("HELMHOLTZ_EQUATION_PROBLEM_SUBTYPE_SET")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_PROBLEM_SUBTYPE_SET",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_PROBLEM_SUBTYPE_SET")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_PROBLEM_SUBTYPE_SET
+
+  !
+  !================================================================================================================================
+  !
+
+  !>Sets up the linear Helmholtz equations solution.
+  SUBROUTINE HELMHOLTZ_EQUATION_PROBLEM_LINEAR_SETUP(PROBLEM,PROBLEM_SETUP,ERR,ERROR,*)
+
+    !Argument variables
+    TYPE(PROBLEM_TYPE), POINTER :: PROBLEM !<A pointer to the problem to setup
+    TYPE(PROBLEM_SETUP_TYPE), INTENT(INOUT) :: PROBLEM_SETUP !<The problem setup information
+    INTEGER(INTG), INTENT(OUT) :: ERR !<The error code
+    TYPE(VARYING_STRING), INTENT(OUT) :: ERROR !<The error string
+    !Local Variables
+    TYPE(VARYING_STRING) :: LOCAL_ERROR
+    
+    CALL ENTERS("HELMHOLTZ_EQUATION_PROBLEM_LINEAR_SETUP",ERR,ERROR,*999)
+
+    IF(ASSOCIATED(PROBLEM)) THEN
+      IF(PROBLEM%SUBTYPE==PROBLEM_NO_SOURCE_HELMHOLTZ_SUBTYPE) THEN
+        SELECT CASE(PROBLEM_SETUP%SETUP_TYPE)
+        CASE(PROBLEM_SETUP_INITIAL_TYPE)
+        CASE(PROBLEM_SETUP_CONTROL_TYPE)
+        CASE(PROBLEM_SETUP_SOLVERS_TYPE)
+        CASE(PROBLEM_SETUP_SOLVER_EQUATIONS_TYPE)
+        CASE DEFAULT
+          LOCAL_ERROR="The setup type of "//TRIM(NUMBER_TO_VSTRING(PROBLEM_SETUP%SETUP_TYPE,"*",ERR,ERROR))// &
+            & " is invalid for a linear Helmholtz equation."
+          CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+        END SELECT
+      ELSE
+        LOCAL_ERROR="The problem subtype of "//TRIM(NUMBER_TO_VSTRING(PROBLEM%SUBTYPE,"*",ERR,ERROR))// &
+          & " does not equal a linear Helmholtz equation subtype."
+        CALL FLAG_ERROR(LOCAL_ERROR,ERR,ERROR,*999)
+      ENDIF
+    ELSE
+      CALL FLAG_ERROR("Problem is not associated.",ERR,ERROR,*999)
+    ENDIF
+    
+    CALL EXITS("HELMHOLTZ_EQUATION_PROBLEM_LINEAR_SETUP")
+    RETURN
+999 CALL ERRORS("HELMHOLTZ_EQUATION_PROBLEM_LINEAR_SETUP",ERR,ERROR)
+    CALL EXITS("HELMHOLTZ_EQUATION_PROBLEM_LINEAR_SETUP")
+    RETURN 1
+  END SUBROUTINE HELMHOLTZ_EQUATION_PROBLEM_LINEAR_SETUP
+
+  !
+  !================================================================================================================================
+  !
+ 
+END MODULE HELMHOLTZ_EQUATIONS_ROUTINES
