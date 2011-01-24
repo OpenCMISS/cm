e629e758c5b9b9b13e201f5a33f1ce366571be1e
Sebastian Krittian <sebastian.krittian@comlab.ox.ac.uk>
2011-01-24 11:43:06 +0000

    Updates on fluid_mechanics_IO_routines after last commit (no special tracker item).
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1912 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- src/fluid_mechanics_IO_routines.f90
+++ src/fluid_mechanics_IO_routines.f90
@@ -2,7 +2,7 @@
 !> $Id$
 !> \author Sebastian Krittian
 !> \brief This module handles some mesh/parameter input routines and cmgui output routines for fluid mechanics
-!> routines and should be eventually replaces by field_IO_routines.f90
+!> routines and should be eventually replaces by field_IO_routines.f90 
 !>
 !> \section LICENSE
 !>
