1a2e3e819a3a72ba1ed5f79286235c3e62cc8196
Zhinuo Jenny Wang <jenny.helyanwe@gmail.com>
2015-04-24 15:52:00 +1200

    Surrounding elements stored in list - improved

--- src/mesh_routines.f90
+++ src/mesh_routines.f90
@@ -1682,11 +1682,10 @@ CONTAINS
     INTEGER(INTG) :: j,ne,ne1,nep1,ni,nic,nn,nn1,nn2,nn3,np,np1,DUMMY_ERR,FACE_XI(2),FACE_XIC(3),NODE_POSITION_INDEX(4)
     INTEGER(INTG) :: xi_direction,direction_index,xi_dir_check,xi_dir_search,NUMBER_NODE_MATCHES
     INTEGER(INTG) :: candidate_idx,face_node_idx,node_idx,surrounding_el_idx,candidate_el,idx
-    INTEGER(INTG) :: NUMBER_SURROUNDING,NUMBER_OF_NODES_XIC(4), count, i
-    INTEGER(INTG), ALLOCATABLE :: NODE_MATCHES(:),ADJACENT_ELEMENTS(:)
+    INTEGER(INTG) :: NUMBER_SURROUNDING,NUMBER_OF_NODES_XIC(4),numberSurroundingElements
+    INTEGER(INTG), ALLOCATABLE :: NODE_MATCHES(:),ADJACENT_ELEMENTS(:), surroundingElements(:)
     LOGICAL :: XI_COLLAPSED,FACE_COLLAPSED(-3:3),SUBSET
-    TYPE(LIST_TYPE), POINTER :: SURROUNDING_ELEMENTS
-    TYPE(LIST_TYPE), POINTER :: NODE_MATCH_LIST
+    TYPE(LIST_TYPE), POINTER :: NODE_MATCH_LIST, surroundingElementsList
     TYPE(LIST_PTR_TYPE) :: ADJACENT_ELEMENTS_LIST(-4:4)
     TYPE(BASIS_TYPE), POINTER :: BASIS
     TYPE(DECOMPOSITION_TYPE), POINTER :: DECOMPOSITION
@@ -1846,11 +1845,11 @@ CONTAINS
                             !Look at the surrounding elements of each of these nodes, if there is a repeated element that
                             !is not the current element ne, it's an adjacent element.
                             candidate_idx=0
-                            NULLIFY(SURROUNDING_ELEMENTS)
-                            CALL LIST_CREATE_START(SURROUNDING_ELEMENTS,ERR,ERROR,*999)
-                            CALL LIST_DATA_TYPE_SET(SURROUNDING_ELEMENTS,LIST_INTG_TYPE,ERR,ERROR,*999)
-                            CALL LIST_INITIAL_SIZE_SET(SURROUNDING_ELEMENTS,2,ERR,ERROR,*999)
-                            CALL LIST_CREATE_FINISH(SURROUNDING_ELEMENTS,ERR,ERROR,*999)
+                            NULLIFY(surroundingElementsList)
+                            CALL LIST_CREATE_START(surroundingElementsList,ERR,ERROR,*999)
+                            CALL LIST_DATA_TYPE_SET(surroundingElementsList,LIST_INTG_TYPE,ERR,ERROR,*999)
+                            CALL LIST_INITIAL_SIZE_SET(surroundingElementsList,2,ERR,ERROR,*999)
+                            CALL LIST_CREATE_FINISH(surroundingElementsList,ERR,ERROR,*999)
                             DO face_node_idx=1,NUMBER_NODE_MATCHES
                               !Dump all the surrounding elements into an array, see if any are repeated
                               node_idx=NODE_MATCHES(face_node_idx)
@@ -1858,22 +1857,14 @@ CONTAINS
                                 candidate_el=DOMAIN_NODES%NODES(node_idx)%SURROUNDING_ELEMENTS(surrounding_el_idx)
                                 IF(candidate_el/=ne) THEN
                                   candidate_idx=candidate_idx+1
-                                  CALL LIST_ITEM_ADD(SURROUNDING_ELEMENTS,candidate_el,ERR,ERROR,*999)
+                                  CALL LIST_ITEM_ADD(surroundingElementsList,candidate_el,ERR,ERROR,*999)
                                 ENDIF
                               ENDDO
                             ENDDO !face_node_idx
+                            CALL LIST_DETACH_AND_DESTROY(surroundingElementsList,numberSurroundingElements,surroundingElements,ERR,ERROR,*999)
                             DO idx=1,candidate_idx
-                              CALL LIST_ITEM_GET(SURROUNDING_ELEMENTS,idx,ne1,ERR,ERROR,*999)
-                              !In a 2D mesh, match 2 nodes, in a 3D mesh, match 3 nodes (line/face)
-                              count = 0
-                              i=1
-                              DO WHILE(i<=SURROUNDING_ELEMENTS%NUMBER_IN_LIST)
-                                IF(SURROUNDING_ELEMENTS%LIST_INTG(i)==ne1) THEN
-                                  count=count+1
-                                ENDIF
-                                i=i+1
-                              ENDDO
-                              IF(count>=BASIS%NUMBER_OF_XI) THEN
+                              ne1=surroundingElements(idx)
+                              IF(COUNT(surroundingElements(1:numberSurroundingElements)==ne1)>=BASIS%NUMBER_OF_XI) THEN
                                 !Found it, just exit
                                 CALL LIST_ITEM_ADD(ADJACENT_ELEMENTS_LIST(xi_direction)%PTR,ne1,ERR,ERROR,*999)
                                 NUMBER_SURROUNDING=NUMBER_SURROUNDING+1
@@ -1882,6 +1873,7 @@ CONTAINS
                             ENDDO
                           ENDIF
                           IF(ALLOCATED(NODE_MATCHES)) DEALLOCATE(NODE_MATCHES)
+                          IF(ALLOCATED(surroundingElements)) DEALLOCATE(surroundingElements)
                         ENDDO !direction_index
                       ENDDO !ni
                     CASE(BASIS_SIMPLEX_TYPE)
